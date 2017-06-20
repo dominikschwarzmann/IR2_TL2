67db585dff3e4b45fabe1bea9fceb97d079263a1
Dominik Schwarzmann <dominik.schwarzmann@stud.uni-bamberg.de>
2017-06-20 13:19:13 +0200

    added indexing of the text files

--- IR2_TL2_Grandl_Schwarzmann/src/Main.java
+++ IR2_TL2_Grandl_Schwarzmann/src/Main.java
@@ -1,8 +1,14 @@
 
+import java.io.IOException;
+
+import org.apache.lucene.queryparser.classic.ParseException;
+
+// https://github.com/apache/lucene-solr/blob/master/lucene/demo/src/java/org/apache/lucene/demo/IndexFiles.java
 public class Main {
 
-	public static void main(String[] args) {
-		System.out.println("Hello World");
-		System.out.println("Goodbye World");
+	public static void main(String[] args) throws IOException, ParseException {
+		Lucene l = new Lucene();
+		l.indexDocuments();
+
 	}
 }
